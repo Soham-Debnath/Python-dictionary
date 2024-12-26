fdccbe37846d8934eb5d4337471e4cc9ffa11fc9
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-27 00:35:09 +0530

    dict_prob

---
+++ 03_dictprob.py
@@ -0,0 +1,21 @@
+# Create a dict of 4 friends with their name as keys and languages as values.
+
+d={}
+
+name1=(input("Enter name1: "))
+lang1=(input("Enter lang1: "))
+d.update({name1:lang1})
+
+name2=(input("Enter name2: "))
+lang2=(input("Enter lang2: "))
+d.update({name2:lang2})
+
+name3=(input("Enter name3: "))
+lang3=(input("Enter lang3: "))
+d.update({name3:lang3})
+
+name4=(input("Enter name4: "))
+lang4=(input("Enter lang4: "))
+d.update({name4:lang4})
+
+print(d)
---
+++ README.md
@@ -0,0 +1,1 @@
+# This codes shows Dictionary
